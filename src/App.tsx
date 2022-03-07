7debf44b554eb59582e09739cf2492d4556b0cb4
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-03-06 23:38:13 -0300

    chore: add useFetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { useFetch } from './hooks/useFetch';
+
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
 export function App() {
-  return <h1>Hello world</h1>;
+  const { data: repositories, isFetching } = useFetch<Repository[]>(
+    'users/arrudadev/repos',
+  );
+
+  return (
+    <ul>
+      {isFetching && <p>Carregando...</p>}
+
+      {repositories?.map(repo => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
